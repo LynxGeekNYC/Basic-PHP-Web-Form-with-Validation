118c6bba7f6e40f4345b327ef32c0b4333afcbcc
LynxGeekNYC <45396441+LynxGeekNYC@users.noreply.github.com>
2023-01-13 15:02:05 -0500

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    // Do something with the form data, like sending an email
+}
+?>
